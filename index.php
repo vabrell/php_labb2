c70b22607d31dc1c7f71b84ebefed6df8bbef5d8
gabbe98 <gabriel.dahlberg1998@gmail.com>
2020-05-26 13:43:01 +0200

    Sista frontend vyn #10 *Frontend åt fotboll, gymnastik, skidor och index sidorna

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once('layouts/header.php');
 
 <div class="container">
     <h2>Startsidan!</h2>
+    <img src="Landsvale.jpg" alt="svala" class="w-100">
 </div>
 
 <?php
--- skidor.php
+++ skidor.php
@@ -1,9 +1,24 @@
 <?php
-require_once('layouts/header.php'); ?>
-<div class="bg-light">
-    <div class="container bg-white">
-        <h2>Skidor!</h2>
-    </div>
+require_once('layouts/header.php');
+
+$_activity = new App\Activity;
+$activities = $_activity->where('name', '=', 'skidor')->get();
+
+$_team = new App\Team;
+$teams = $_team->where('activity_id', '=', $activities[0]->id)->get();
+
+?>
+<div class="container">
+    <h1>Skidor</h1>
+        <?php 
+            foreach ($teams as $team) {
+                echo '<div><h3>' . $team->name . '</h3><ul>';
+                foreach ($team->members() as $member) {
+                    echo '<li>' . $member->firstName . ' ' . $member->lastName . '</li>';
+                }
+                echo '</ul></div>';
+            }
+        ?>
 </div>
 <?php
 require_once('layouts/footer.php');
